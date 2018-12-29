78de006d8a68b4d85381d96536d22b70ee90deed
eric <zhengly123@foxmail.com>
2018-12-29 23:30:18 +0800

    Add: gtest case of Update

--- src/gtestcase/QL1.cpp
+++ src/gtestcase/QL1.cpp
@@ -335,3 +335,158 @@ TEST_F(QL_DEL, DEL)
         EXPECT_EQ(rc, 0);
     }
 }
+
+TEST_F(QL_DEL, UPD)
+{
+    printf("\nTesting : Insert Non-null Type Error. \n");
+    freopen("../src/gtestcase/QL_UPDATE1.in","r",stdin);
+
+    Printer *printer=new StdoutPrinter;
+    FileManager* fm = new FileManager();
+    BufPageManager* bpm = new BufPageManager(fm);
+    RM_Manager rmManager(fm, bpm);
+    IX_Manager ixManager(*fm, *bpm);
+    SM_Manager smManager(ixManager, rmManager, printer); // add printer
+    QL_Manager qlManager(smManager, ixManager, rmManager, printer);// add printer
+
+    int rc;
+
+    for(int i = 0; i < 4; ++ i)
+    {
+        rc = treeparser(smManager, qlManager, 0);
+        EXPECT_EQ(rc, 0);
+    }
+    //show 1
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    auto type=vector<string>();
+    type.push_back(string("INT"));
+    type.push_back(string("INT"));
+    string s;
+    int t;
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    int a[6] = {1, 3, 2, 4, 3, 5};
+    for (int i = 0; i < 6; ++i)
+    {
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+    //show 2
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    for (int i = 0; i < 6; ++i)
+    {
+        if (i == 0)
+        {
+            printer->getSS()>>s;
+            EXPECT_EQ(string("NULL"), s);
+            continue;
+        }
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+    //show 3
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    a[2] = 123;
+    for (int i = 0; i < 6; ++i)
+    {
+        if (i == 0)
+        {
+            printer->getSS()>>s;
+            EXPECT_EQ(string("NULL"), s);
+            continue;
+        }
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+    //show 4
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    a[3] = 5;
+    for (int i = 0; i < 6; ++i)
+    {
+        if (i == 0)
+        {
+            printer->getSS()>>s;
+            EXPECT_EQ(string("NULL"), s);
+            continue;
+        }
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+    //show 5
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    for (int i = 0; i < 6; ++i)
+    {
+        if (i == 0 || i == 3 || i == 5)
+        {
+            printer->getSS() >> s;
+            EXPECT_EQ(string("NULL"), s);
+            continue;
+        }
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+    //show 6
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    rc = treeparser(smManager, qlManager, 0);
+    EXPECT_EQ(rc, 0);
+    for (int i = 0; i < 2; ++i)
+    {
+        printer->getSS()>>s;
+        ASSERT_EQ(s,type[i]);
+    }
+    for (int i = 0; i < 6; ++i)
+    {
+        if (i == 0 || i == 3 || i == 5)
+        {
+            printer->getSS() >> s;
+            EXPECT_EQ(string("NULL"), s);
+            continue;
+        }
+        printer->getSS()>>t;
+        EXPECT_EQ(t,a[i]);
+    }
+
+    for(;;)
+    {
+        rc = treeparser(smManager, qlManager, 0);
+        if(rc == PARSEREXIT) break;
+        EXPECT_EQ(rc, 0);
+    }
+}
